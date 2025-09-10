205b1786f5f975c42daee58370d01f26d9841b22
H82M19\Student <Student>
2025-09-10 08:40:35 +0700

    4. Finish done cau 2

--- Cau1.tsx
+++ Cau1.tsx
@@ -1,31 +1,68 @@
 import { Text, SafeAreaView, View, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
-
+import {useState} from 'react'
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={{width: 300, border: "1px solid #000", padding: 10}}>Ket qua</Text>
-      <TextInput style={styles.input} placeholder="Nhap so A" />
-      <TextInput  style={styles.input}  placeholder="Nhap so B" />
+  const [inputA, setInputA] = useState("");
+  const [inputKQ, setInputKQ] = useState("0");
+   const handleClick = (value) => {
+    switch (value){
+      case "Enter":
+        {
+          if (inputA.includes("+")){
+            setInputKQ("A")
+            const kq = parseInt( inputA.split("+")[0]) + parseInt( inputA.split("+")[1])
+            setInputKQ(kq + "");
+          }  else if (inputA.includes("-")){
+            const kq = parseInt( inputA.split("-")[0]) - parseInt( inputA.split("-")[1])
+             setInputKQ(kq + "");
+          } else if (inputA.includes("*")){
+            const kq = parseInt( inputA.split("*")[0]) * parseInt( inputA.split("*")[1])
+             setInputKQ(kq + "");
+          } else if (inputA.includes(":")){
+            const kq = parseInt( inputA.split(":")[0])/parseInt( inputA.split(":")[1])
+             setInputKQ(kq + "");
+          }
+          break;
+        }
+      case "Xoa": 
+      {
+        setInputA(inputA.substring(0, inputA.length - 1))
+        break;
+      }
+      default: 
+        {
+          setInputA(inputA + value);
+          break;
+        }
+    }
+    
+  }
 
+
+  return ( 
+    <SafeAreaView style={styles.container}>
+      <Text style={{width: 300, border: "1px solid #000", padding: 10}}>{inputKQ}</Text>
+      <TextInput value={inputA} onChangeText={setInputA} style={styles.input} placeholder="Nhap o day" />
       <View style={styles.flexRow}> 
-        <TouchableOpacity style={styles.button}>0 </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>1 </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>2 </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>3 </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>4 </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>5 </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>6 </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>7 </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>8 </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>9 </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleClick("0")} style={styles.button}>0 </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleClick("1")} style={styles.button}>1 </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleClick("2")} style={styles.button}>2 </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleClick("3")} style={styles.button}>3 </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleClick("4")} style={styles.button}>4 </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleClick("5")} style={styles.button}>5 </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleClick("6")} style={styles.button}>6 </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleClick("7")} style={styles.button}>7 </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleClick("8")} style={styles.button}>8 </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleClick("9")} style={styles.button}>9 </TouchableOpacity>
       </ View>
        <View style={styles.flexRow}> 
-        <TouchableOpacity style={styles.button}>+ </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>- </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>* </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>: </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>Xoa</TouchableOpacity>
+        <TouchableOpacity  onPress={() => handleClick("+")} style={styles.button}>+ </TouchableOpacity>
+        <TouchableOpacity  onPress={() => handleClick("-")} style={styles.button}>- </TouchableOpacity>
+        <TouchableOpacity  onPress={() => handleClick("*")} style={styles.button}>* </TouchableOpacity>
+        <TouchableOpacity  onPress={() => handleClick(":")} style={styles.button}>: </TouchableOpacity>
+        <TouchableOpacity  onPress={() => handleClick("Xoa")} style={styles.button}>Xoa</TouchableOpacity>
+        <TouchableOpacity  onPress={() => handleClick("Enter")} style={styles.button}>Enter</TouchableOpacity>
+
       </ View>
     </SafeAreaView>
   );
